535db0bdfc4acadca4b2e94effeb975a12425c8c
Abdulrahman84 <55898084+Abdulrahman84@users.noreply.github.com>
2021-06-03 23:20:05 +0300

    cors

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const swaggerDocument = require("./swagger.json");
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
-app.use(cors());
 
 env.config();
 // mongobb connection
